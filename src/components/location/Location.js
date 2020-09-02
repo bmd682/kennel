e38996f44c21ad2da7573eb8810f5b8a02a09d9f
Brett Derrington <brett.derrington@gmail.com>
2020-09-02 11:40:02 -0500

    Starting react files and chapters 1 and 2 of the React component.

---
+++ src/components/location/Location.js
@@ -0,0 +1,8 @@
+import React from "react"
+import "./Location.css"
+
+export const Location = () => (
+    <section className="location">
+        <h3 className="location__name">Brentwood</h3>
+    </section>
+)
